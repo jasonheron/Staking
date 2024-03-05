164d4c9e266decaf0c0ac61f388ba930828a41a7
Jason Heron <jasonheron@mail.com>
2024-03-05 10:41:31 +0000

    Update get-fixtures.js

--- src/api/get-fixtures.js
+++ src/api/get-fixtures.js
@@ -33,7 +33,7 @@ export default async (req, res) => {
         const currentRound = getCurrentRound();
         const season = 2023; // Adjust season as needed
 
-        const response = await axios.get(`https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=${season}&round=Regular%20Season%20-%20${currentRound}`, {
+        const response = await axios.get(`https://v3.football.api-sports.io/fixtures?league=39&season=${season}&round=Regular%20Season%20-%20${currentRound}`, {
             headers: {
                 'X-RapidAPI-Key': '126ab6d01ffa281853d1ae19f4c70a46'
             }
