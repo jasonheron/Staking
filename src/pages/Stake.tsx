d06f34cfafdc07a799a2ad31c9ab5e099f3ff2b9
Jason Heron <jasonheron@mail.com>
2024-03-05 14:20:05 +0000

    Update Stake.tsx

--- src/pages/Stake.tsx
+++ src/pages/Stake.tsx
@@ -101,7 +101,7 @@ const Stake = () => {
         let matchesData : any;
       
         
-      await axios.get(`https://v3.football.api-sports.io/fixtures?league=39&season=2023&round=Regular%20Season%20-%2027`,{
+        await axios.get(`https://v3.football.api-sports.io/fixtures?league=39&season=2023&round=Regular Season - 27`,{
           headers: {
             'X-RapidAPI-Key' : '126ab6d01ffa281853d1ae19f4c70a46'
           }
