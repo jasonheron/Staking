6000bf25a44b3154e2d55642c67082725f20dd9d
Jason Heron <jasonheron@mail.com>
2024-03-23 12:22:15 +0000

    updated rpc

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -5,6 +5,6 @@ import { Connection,
 // export const connection = new Connection("https://api.mainnet-beta.solana.com", 'processed');
 // export const connection = new Connection("https://devnet.helius-rpc.com/?api-key=fb8d7b85-2e9c-4121-9018-c1f9a13fedcd", 'processed');
 // export const connection = new Connection("https://explorer-api.devnet.solana.com", 'processed');
-export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=c94aaeb1-2ddf-45d6-a38d-383b3e6d1a64", 'processed');
+export const connection = new Connection("https://solana-mainnet.g.alchemy.com/v2/fTOy7dFQa8qxsHPNORjmEqUlmIjM7_sP", 'processed');
 
 // export const connection = new Connection(clusterApiUrl("devnet"), 'processed');
