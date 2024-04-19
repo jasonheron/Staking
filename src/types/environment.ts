32b49ada6b8ca64d0bd7d262a7518b495f18e18d
Jason Heron <jasonheron@mail.com>
2024-04-19 20:19:40 +0100

    Revert "test rpc"
    This reverts commit fd280fd16718022ee29a74a05f727e5032b8fbb0.

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -5,6 +5,6 @@ import { Connection,
 // export const connection = new Connection("https://api.mainnet-beta.solana.com", 'processed');
 // export const connection = new Connection("https://devnet.helius-rpc.com/?api-key=fb8d7b85-2e9c-4121-9018-c1f9a13fedcd", 'processed');
 // export const connection = new Connection("https://explorer-api.devnet.solana.com", 'processed');
-export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=2a30684d-aa5e-4567-8399-47350223", 'processed');
+export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=c94aaeb1-2ddf-45d6-a38d-383b3e6d1a64", 'processed');
 
 // export const connection = new Connection(clusterApiUrl("devnet"), 'processed');
