4a9cabd2e556b9354dc1687575b179943a1d8820
Jason Heron <jasonheron@mail.com>
2024-04-19 19:37:06 +0100

    Update envi..ts -
    test rpc

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -5,6 +5,6 @@ import { Connection,
 // export const connection = new Connection("https://api.mainnet-beta.solana.com", 'processed');
 // export const connection = new Connection("https://devnet.helius-rpc.com/?api-key=fb8d7b85-2e9c-4121-9018-c1f9a13fedcd", 'processed');
 // export const connection = new Connection("https://explorer-api.devnet.solana.com", 'processed');
-export const connection = new Connection("https://rpc.hellomoon.io/a96b4c1a-c20e-4a79-9546-bab4065994e0", 'processed');
+export const connection = new Connection("https://rpc.hellomoon.io/2a30684d-aa5e-4567-8399-47350223", 'processed');
 
 // export const connection = new Connection(clusterApiUrl("devnet"), 'processed');
