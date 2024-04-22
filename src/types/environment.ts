0e49298dafaf0f1205817aa3ad16d81030a50cd6
Jason Heron <jasonheron@mail.com>
2024-04-22 11:24:23 +0100

    revert

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -5,6 +5,10 @@ import { Connection,
 // export const connection = new Connection("https://api.mainnet-beta.solana.com", 'processed');
 // export const connection = new Connection("https://devnet.helius-rpc.com/?api-key=fb8d7b85-2e9c-4121-9018-c1f9a13fedcd", 'processed');
 // export const connection = new Connection("https://explorer-api.devnet.solana.com", 'processed');
+<<<<<<< HEAD
 export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=31a46afe-c5fe-4e8b-a849-3f9c8dc3fc30", 'processed');
+=======
+export const connection = new Connection("https://solana-mainnet.g.alchemy.com/v2/fTOy7dFQa8qxsHPNORjmEqUlmIjM7_sP", 'processed');
+>>>>>>> parent of a26eba36f (Revert "updated rpc")
 
 // export const connection = new Connection(clusterApiUrl("devnet"), 'processed');
