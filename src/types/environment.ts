248d226b5f43a44f39734d8e61747e713eec5594
Jason Heron <jasonheron@mail.com>
2024-05-10 20:08:06 +0100

    Full Overhall

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -5,6 +5,6 @@ import { Connection,
 // export const connection = new Connection("https://api.mainnet-beta.solana.com", 'processed');
 // export const connection = new Connection("https://devnet.helius-rpc.com/?api-key=fb8d7b85-2e9c-4121-9018-c1f9a13fedcd", 'processed');
 // export const connection = new Connection("https://explorer-api.devnet.solana.com", 'processed');
-export const connection = new Connection("https://rpc.hellomoon.io/a96b4c1a-c20e-4a79-9546-bab4065994e0", 'processed');
+export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=31a46afe-c5fe-4e8b-a849-3f9c8dc3fc30", 'processed');
 
 // export const connection = new Connection(clusterApiUrl("devnet"), 'processed');
